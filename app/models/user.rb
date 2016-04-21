fdd5785b979bf6b54cdc926607bdd7d19a684a25
Zachary Uzupis <zuzupis@gmail.com>
2016-04-21 16:09:19 -0400

    added button css to home index

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,4 @@ class User < ActiveRecord::Base
    validates_presence_of :username, :email
    validates_uniqueness_of :username, :email
 
-   # users.password_hash in the database is a :string
-
-  # include BCrypt
-
-  # def password
-  #  @password ||= Password.new(password_hash)
-  # end
-
-  # def password=(new_password)
-  #  @password = Password.create(new_password)
-  #  self.password_hash = @password
-  # end
 end
